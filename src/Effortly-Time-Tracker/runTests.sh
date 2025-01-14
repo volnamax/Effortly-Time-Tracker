88b6c2a5349a5d99b70b127ad32ac37e154977d4
max <mvolnyaga@gmail.com>
2025-01-14 12:11:33 +0300

    хз

--- src/Effortly-Time-Tracker/runTests.sh
+++ src/Effortly-Time-Tracker/runTests.sh
@@ -4,9 +4,9 @@ echo "Running Unit Tests..."
 mvn clean test -Punit-tests || exit 1
 
 echo "Running Integration Tests..."
-mvn clean test -Pintegration-tests || exit 1
+mvn  test -Pintegration-tests || exit 1
 
 echo "Running E2E Tests..."
-mvn clean verify -Pe2e-tests || exit 1
+mvn  verify -Pe2e-tests || exit 1
 
 echo "All tests completed successfully."
